867a55180fe5551bcbb68df05d176883961b04ce
ricklon <rianders@docs.rutgers.edu>
2023-03-17 18:55:32 -0400

    Added ability to change play list name

--- main.py
+++ main.py
@@ -12,17 +12,19 @@ load_dotenv()
 SPOTIPY_CLIENT_ID = os.environ["SPOTIPY_CLIENT_ID"]
 SPOTIPY_CLIENT_SECRET = os.environ["SPOTIPY_CLIENT_SECRET"]
 SPOTIPY_REDIRECT_URI = os.environ["SPOTIPY_REDIRECT_URI"]
-SPOTIFY_USERNAME = os.environ["SPOTIFY_USERNAME"]
+
 # Authenticate with Spotify API
 scope = "playlist-modify-public playlist-modify-private"
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
 
-# Replace with your Spotify username
-username = SPOTIFY_USERNAME
+# Get the current user's profile information
+user_profile = sp.current_user()
 
-def import_playlist(csv_file):
+# Get the user's Spotify ID
+username = user_profile["id"]
+
+def import_playlist(playlist_name, csv_file):
     # Create a new Spotify playlist
-    playlist_name = "My Imported Playlist"
     playlist_description = "Playlist imported from a CSV file"
     new_playlist = sp.user_playlist_create(username, playlist_name, description=playlist_description)
 
@@ -49,7 +51,10 @@ def import_playlist(csv_file):
 # Gradio interface
 iface = gr.Interface(
     fn=import_playlist,
-    inputs=gr.inputs.Textbox(lines=18, label="Paste CSV Content"),
+    inputs=[
+        gr.inputs.Textbox(default="My Imported Playlist", label="Playlist Name"),
+        gr.inputs.Textbox(lines=18, label="Paste CSV Content")
+    ],
     outputs="text",
     title="CSV to Spotify Playlist",
     description="Import a playlist from a CSV file to your Spotify account."
